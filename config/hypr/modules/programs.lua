16fcb25902235fb090f41c52a7cd54a1b3087a55
Axel Rhelimi <axel.rhelimi@gmail.com>
2026-09-24 15:39:42 +0200

    chore: update desktop configuration

--- config/hypr/modules/programs.lua
+++ config/hypr/modules/programs.lua
@@ -1,6 +1,6 @@
 local M = {
     terminal = "kitty",
-    file_manager = "dolphin",
+    fileManager = "dolphin",
     browser = "firefox",
     menu = "rofi -show drun",
     launcher = "wofi --show drun",
